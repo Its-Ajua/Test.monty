7769052b4eaaa870e5d21c3ee46c200e409aa8a4
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-22 11:03:43 +0100

    Test

---
+++ padd.c
@@ -0,0 +1,24 @@
+void p_add(stact_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int amount = 0, temp;
+
+	h = *head;
+
+	while (h)
+	{
+		h = h->next;
+		amount++;
+	}
+
+	if (amount < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short", amount);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = h->n + h->next->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
---
+++ pop.c
@@ -0,0 +1,15 @@
+void p_op(stack_t **head, int unsigned counter)
+{
+	stack_t *h;
+
+	h = *head;
+
+	if (h == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack", counter);
+		exit(EXIT_FAILURE);
+	}
+
+	h = h->next;
+	free(h);
+}
