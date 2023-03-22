408e301678722746fe7a84de27b940d148a2e342
Its-Ajua <akinladerebecca04@gmail.com>
2023-03-22 10:49:33 +0100

    Test

---
+++ pall.c
@@ -0,0 +1,16 @@
+void p_all(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	(void) counter;
+
+	h = *head;
+
+	if (h == NULL)
+		return;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+}
